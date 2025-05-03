0f2d8f21c6f8da068ca182f13ca742e6e78ee73a
slow-wave-repo <teodantes@gmail.com>
2025-05-03 19:04:54 +0200

    Label Widget in Kivy

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
-import kivy
-kivy.require('1.10.0')
+from PIL.ImageChops import screen
+from kivymd.app import MDApp
+from kivymd.uix.button.button import theme_text_color_options
+from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import Screen
 
-from kivy.app import App
-from kivy.uix.button import Label
 
-class MyLabelApp(App):
+class Demo(MDApp):
 
     def build(self):
-        lbl = Label(text="Label added!",
-                    font_size='20sp',
-                    color=(0.41, 0.42, 0.74, 1)
-                    )
-        return lbl
+        screen = Screen()
+
+        lbl = MDLabel(
+            text="Welcome!",
+            pos_hint={
+                'center_x':0.8,
+                'center_y':0.8
+            },
+            theme_text_color="Custom",
+            text_color=(0.5,0,0.5,1),
+            font_style='Caption'
+        )
+
+        lbl1 = MDLabel(
+            text="Welcome!",
+            pos_hint={
+                'center_x': 0.8,
+                'center_y': 0.5
+            },
+            theme_text_color="Custom",
+            text_color=(0.5, 0, 0.5, 1),
+            font_style='H2'
+        )
+
+        lbl2 = MDLabel(
+            text="Welcome!",
+            pos_hint={
+                'center_x': 0.8,
+                'center_y': 0.2
+            },
+            theme_text_color="Custom",
+            text_color=(0.5, 0, 0.5, 1),
+            font_style='H1'
+        )
+
+        screen.add_widget(lbl)
+        screen.add_widget(lbl1)
+        screen.add_widget(lbl2)
+
+        return screen
 
 
-label = MyLabelApp()
-label.run()
+if __name__ == "__main__":
+    Demo().run()

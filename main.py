d96e2b08b99bed89451b050db416651b41464eae
slow-wave-repo <teodantes@gmail.com>
2025-05-06 00:27:51 +0200

    Scroll View - 2

--- main.py
+++ main.py
@@ -1,37 +1,17 @@
-# python #main.py
+#python #main.py
 
 
 import kivy
 
 kivy.require('1.9.0')
 
-from kivy.uix.scrollview import ScrollView
-from kivy.properties import StringProperty
-from kivy.base import runTouchApp
+from kivy.app import App
 from kivy.lang import Builder
 
-Builder.load_string('''
-<ScrollableLabel>:
-    text: 'You are learning Kivy\\n' * 100
-    do_scroll_x: False
-    do_scroll_y: True
+class uiApp(App):
+    def build(self):
 
-    BoxLayout:
-        orientation: 'vertical'
-        size_hint_y: None
-        height: self.minimum_height
-
-        Label:
-            text: root.text
-            font_size: 20
-            text_size: self.width, None
-            size_hint_y: None
-            height: self.texture_size[1]
-''')
+        return Builder.load_file('./kv/scroll.kv')
 
 
-class ScrollableLabel(ScrollView):
-    text = StringProperty('')
-
-
-runTouchApp(ScrollableLabel())
+uiApp().run()
